240562e1140a3355d9c0295b61c92bdc0e74134a
HMJ <hmj@goatpage.com>
2022-06-26 22:31:27 +0200

    peers

--- components/Main.vue
+++ components/Main.vue
@@ -1,8 +1,11 @@
 <script setup>
+
+const streams = usePeersStreams();
+
 onMounted(() => {
     setTimeout(()=>{ 
         MediaControl.show();
-  MediaControl.getDevices();},100)
+        MediaControl.getDevices();},100)
 
 
 });
@@ -10,9 +13,11 @@ onMounted(() => {
 <template>
   <i-row center style="background-image:url('./background.jpg');background-size:100%;height:85vh;padding:10px;margin:0px" >
     <i-column xs="12" md="8" lg="6" color="dark" >
-        <div >
-      <MediaVideoSelf />
-      </div>
+    <div style="margin: 3px"
+      v-for="(item, index) in streams"
+      :key="'stream_' + index">
+        <MediaVideo :stream="item.stream" />
+    </div>
     </i-column>
         <i-column xs="12" md="2" lg="2" >
             <div  >
